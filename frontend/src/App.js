5fd869a59df051b83cad3f3ff5ab278aabf5c101
ChuongTang <chuong.tang@aol.com>
2021-05-13 23:09:28 -0600

    prior to adding react-router S2vi8

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 
 
-
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
-import Footer from './components/Footer.jsx'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
@@ -11,7 +11,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome To ProShop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
