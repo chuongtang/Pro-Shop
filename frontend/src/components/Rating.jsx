c55af0c4dd66e6ce770be078943085be89f26bcb
ChuongTang <chuong.tang@aol.com>
2021-05-16 13:23:25 -0600

    customise to cute shop

--- frontend/src/components/Rating.jsx
+++ frontend/src/components/Rating.jsx
@@ -4,64 +4,59 @@ import PropTypes from 'prop-types';
 
 const Rating = ({ value, text, color }) => {
   return (
-    <div className='rating'>
+    <div className='rating' style={{ color: 'pink' }}>
       <span>
         <i
-          style={{ color }}
           className={
             value >= 1
-              ? 'fas fa-star'
+              ? 'fas fa-cookie'
               : value >= 0.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
+                ? 'fas fa-cookie-bite'
+                : 'far fa-cookie'
           }
         ></i>
       </span>
       <span>
         <i
-          style={{ color }}
           className={
             value >= 2
-              ? 'fas fa-star'
+              ? 'fas fa-cookie'
               : value >= 1.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
+                ? 'fas fa-cookie-bite'
+                : 'far fa-cookie'
           }
         ></i>
       </span>
       <span>
         <i
-          style={{ color }}
           className={
             value >= 3
-              ? 'fas fa-star'
+              ? 'fas fa-cookie'
               : value >= 2.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
+                ? 'fas fa-cookie-bite'
+                : 'far fa-cookie'
           }
         ></i>
       </span>
       <span>
         <i
-          style={{ color }}
           className={
             value >= 4
-              ? 'fas fa-star'
+              ? 'fas fa-cookie'
               : value >= 3.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
+                ? 'fas fa-cookie-bite'
+                : 'far fa-cookie'
           }
         ></i>
       </span>
       <span>
         <i
-          style={{ color }}
           className={
             value >= 5
-              ? 'fas fa-star'
+              ? 'fas fa-cookie'
               : value >= 4.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
+                ? 'fas fa-cookie-bite'
+                : 'far fa-cookie'
           }
         ></i>
       </span>
