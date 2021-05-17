aa438070c2d9c6439c8b3289de30b430b16f49a1
ChuongTang <chuong.tang@aol.com>
2021-05-16 22:37:11 -0600

    added profile and get User detail

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -18,14 +18,14 @@ const Header = () => {
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>ProShop</Navbar.Brand>
+            <Navbar.Brand>Pro-cuteShop</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
               <LinkContainer to='/cart'>
                 <Nav.Link>
-                  <i className='fas fa-shopping-cart'></i> Cart
+                  <i className='fas fa-baby-carriage'></i> cuteCart
                 </Nav.Link>
               </LinkContainer>
               {userInfo ? (
@@ -40,7 +40,7 @@ const Header = () => {
               ) : (
                 <LinkContainer to='/login'>
                   <Nav.Link>
-                    <i className='fas fa-user'></i> Sign In
+                    <i className='fas fa-baby'></i>cuteSign-In
                   </Nav.Link>
                 </LinkContainer>
               )}
