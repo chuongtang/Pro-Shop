af465702be2209aac1280d8eb51123b27440e84d
ChuongTang <chuong.tang@aol.com>
2021-05-19 18:25:49 -0600

    added UserList reset for adminLOGout

--- frontend/src/screens/UserListScreen.jsx
+++ frontend/src/screens/UserListScreen.jsx
@@ -6,15 +6,22 @@ import Message from '../components/Message';
 import Loader from '../components/Loader';
 import { listUsers } from '../actions/userActions';
 
-const UserListScreen = () => {
+const UserListScreen = ({ history }) => {
   const dispatch = useDispatch();
 
   const userList = useSelector((state) => state.userList);
   const { loading, error, users } = userList;
 
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
   useEffect(() => {
-    dispatch(listUsers());
-  }, [dispatch]);
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listUsers());
+    } else {
+      history.push('/login');
+    }
+  }, [dispatch, history]);
 
   const deleteHandler = (id) => {
     console.log('delete');
@@ -48,11 +55,7 @@ const UserListScreen = () => {
                   <a href={`mailto:${user.email}`}>{user.email}</a>
                 </td>
                 <td>
-                  {user.isAdmin ? (
-                    <i className='fas fa-check' style={{ color: 'green' }}></i>
-                  ) : (
-                    <i className='fas fa-times' style={{ color: 'red' }}></i>
-                  )}
+                  {user.isAdmin ? <p>✅</p> : <p>❌</p>}
                 </td>
                 <td>
                   <LinkContainer to={`/user/${user._id}/edit`}>
