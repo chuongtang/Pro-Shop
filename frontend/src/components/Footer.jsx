9ac997dff7a5c72ad499c70eba317624b405e4c8
ChuongTang <chuong.tang@aol.com>
2021-05-23 17:45:06 -0600

    added Favicon@footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,12 +1,13 @@
-
 import { Container, Row, Col } from 'react-bootstrap';
+import Image from './FaviconCLear.png';
 
 const Footer = () => {
   return (
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Copyright &copy; Pro👶Shop</Col>
+          <Col className='text-center py-3'>Copyright &copy; Pro👶Shop and <img src={Image} alt="FaviconImage" style={{ maxHeight: '4vh' }} /> , a junior developer</Col>
+
         </Row>
       </Container>
     </footer>
