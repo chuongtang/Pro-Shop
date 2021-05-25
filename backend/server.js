2135351980c105611e5c209581714c205d080cb2
ChuongTang <chuong.tang@aol.com>
2021-05-24 20:41:29 -0600

    make it better

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
-import morgan from 'morgan'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 
@@ -16,10 +15,6 @@ dotenv.config();
 connectDB();
 
 const app = express();
-
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
 
 
 app.use(express.json());
